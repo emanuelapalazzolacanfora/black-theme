83dbc3970179fb277104106a525008b71b7945a6
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2023-12-18 04:49:36 +0100

    Add files via upload

--- mobilnav.js
+++ mobilnav.js
@@ -1,13 +1,16 @@
 const allNavBtn = document.querySelectorAll('nav ul li');
-const btnBurger = document.querySelector('#menu-icon');
 
-btnBurger.addEventListener('click', () => {
-    document.querySelector('nav').style.height = '';
-})
 
-allNavBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        document.querySelector('nav').style.height = 0;
-        document.querySelector('#close-icon').click();
-    })
-});
+
+    allNavBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            document.getElementById("check").checked = false;
+        })
+    });
+
+
+
+
+
+
+
